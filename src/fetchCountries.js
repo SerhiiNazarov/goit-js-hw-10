4bc92572e53a11c058932b9b088bb5e11e582991
SerhiiNazarov <algazira89@gmail.com>
2022-10-08 18:43:26 +0300

    add notiflix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,12 @@ export function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,name.official,capital,population,flags,languages`;
   return fetch(url).then(response => {
     if (!response.ok) {
-      throw new Error('Oops, there is no country with that name');
+      throw new Error('Oops, there is no country with that name', {
+        position: 'center-center',
+        cssAnimationStyle: 'from-top',
+        showOnlyTheLastOne: true,
+        clickToClose: true,
+      });
     }
     return response.json();
   });
--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ refs.inputRef.addEventListener('input', debounce(onInputCLick, DEBOUNCE_DELAY));
 function onInputCLick() {
   const searchQuery = refs.inputRef.value.trim().toLowerCase();
   if (!searchQuery) {
-    Notiflix.Notify.failure('Введіть дані для пошуку');
+    Notiflix.Notify.failure('Введіть дані для пошуку', {
+      position: 'center-center',
+      cssAnimationStyle: 'from-top',
+      showOnlyTheLastOne: true,
+      clickToClose: true,
+    });
     return;
   }
 
@@ -23,7 +28,13 @@ function onInputCLick() {
       clearInfoList();
       if (data.length > 10) {
         Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
+          'Too many matches found. Please enter a more specific name.',
+          {
+            position: 'center-center',
+            cssAnimationStyle: 'from-top',
+            showOnlyTheLastOne: true,
+            clickToClose: true,
+          }
         );
         return;
       }
@@ -36,13 +47,18 @@ function onInputCLick() {
       refs.listRef.insertAdjacentHTML('beforeend', createMarkup(data));
     })
     .catch(error => {
-      Notiflix.Notify.failure(error.message, 'Something is wrong !');
+      Notiflix.Notify.failure(error.message, 'Something is wrong !', {
+        position: 'center-center',
+        cssAnimationStyle: 'from-top',
+        showOnlyTheLastOne: true,
+      });
       clearInfoList();
     });
 }
 
 refs.inputRef.style.fontSize = '24px';
 refs.inputRef.style.borderColor = 'red';
+refs.inputRef.style.outline = 'none';
 refs.inputRef.style.backgroundColor = '#D9FCF1 ';
 refs.inputRef.style.borderRadius = '10px';
 refs.listRef.style.listStyle = 'none';
